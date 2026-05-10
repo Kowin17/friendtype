d8ee70aeeb86089bbf02184b3956fd05c52174a5
Kowin17 <Kowin2004@outlook.com>
2026-05-09 18:35:34 -0700

    Add files via upload

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
-import "./globals.css";
+import './globals.css';
 
-export const metadata = { title: "FriendType", description: "友情人格测试" };
+export const metadata = {
+  title: 'FriendType',
+  description: '友情人格测试',
+};
 
 export default function RootLayout({ children }) {
   return (
